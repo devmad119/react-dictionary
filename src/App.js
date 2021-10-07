6a08f3758bdc622efd782132017e09ba1d50e7fb
kiddodev050 <yt1997119@gmail.com>
2021-10-07 09:29:03 +0530

    adding all the components
    such as header, Definition, Theme toggle

--- src/App.js
+++ src/App.js
@@ -1,10 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Header from "./components/Header";
+import ThemeToggle from "./components/ThemeToggle";
+import Definition from "./components/Definition";
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    const [meanings, setMeanings] = useState([]);
+    const [word, setWord] = useState("");
+    const [category, setCategory] = useState("en");
+
+    const BASE_URL = "https://api.dictionaryapi.dev/api/v2/entries/";
+
+    useEffect(() => {
+        async function dictionaryAPI() {
+            try {
+                if (word !== "") {
+                    const fetchedData = await axios.get(
+                        BASE_URL + `${category}/${word}`
+                    );
+                    setMeanings(fetchedData.data);
+                }
+            } catch (err) {
+                console.error(err);
+            }
+        }
+        dictionaryAPI();
+    }, [word, category]);
+
+    return (
+        <React.Fragment>
+            <Header
+                category={category}
+                setCategory={setCategory}
+                word={word}
+                setWord={setWord}
+            />
+            {word && (
+                <Definition
+                    meanings={meanings}
+                    word={word}
+                    category={category}
+                />
+            )}
+            <ThemeToggle />
+        </React.Fragment>
+    );
 }
 
 export default App;
