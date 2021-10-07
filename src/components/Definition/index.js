c400bda10a909e4219b59d60d26b7a449b84bab2
devilhacker0119 <devilhacker0119@gmail.com>
2021-10-07 13:43:27 +0530

    replacing audio with the speaker image

--- src/components/Definition/index.js
+++ src/components/Definition/index.js
@@ -1,18 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import "./definition.css";
-const index = ({ meanings, word, category }) => {
+
+const Index = ({ meanings, word, category }) => {
+    const [opacity, setOpacity] = useState({
+        opacity: ".5",
+    });
+
+    function handlePlay() {
+        document.querySelector(".speaker").addEventListener("click", () => {
+            const audio = document.getElementsByTagName("audio")[0];
+            audio.play();
+            setOpacity({ opacity: "1" });
+            setTimeout(() => {
+                setOpacity({ opacity: ".5" });
+            }, audio.duration * 1000);
+        });
+    }
+
     return (
         <div className="meanings_container">
             {meanings[0] && word && category === "en" && (
-                <audio
-                    style={{ borderRadius: "0 !important" }}
-                    src={
-                        meanings[0].phonetics[0] &&
-                        meanings[0].phonetics[0].audio
-                    }
-                    controls
-                ></audio>
+                <div className="audio-container">
+                    <audio
+                        style={{ borderRadius: "0 !important" }}
+                        src={
+                            meanings[0].phonetics[0] &&
+                            meanings[0].phonetics[0].audio
+                        }
+                        controls
+                    ></audio>
+
+                    <div
+                        style={opacity}
+                        className="speaker"
+                        onClick={handlePlay}
+                    ></div>
+                </div>
             )}
+
+            {meanings.map((word) => {
+                return (
+                    word.origin && (
+                        <div className="origin">
+                            <p>
+                                Origin : <span>{word.origin}</span>
+                            </p>
+                        </div>
+                    )
+                );
+            })}
 
             {meanings.map((mean) => {
                 return mean.meanings.map((item) => {
@@ -32,13 +68,19 @@ const index = ({ meanings, word, category }) => {
                                         {def.antonyms.length > 0 && (
                                             <p className="antonyms">
                                                 <b> Antonyms : </b>
-                                                {def.antonyms.join(", ")}
+                                                {/* {def.antonyms.join(", ")} */}
+                                                {def.antonyms.map((item) => {
+                                                    return <p>{item}</p>;
+                                                })}
                                             </p>
                                         )}
                                         {def.synonyms.length > 0 && (
                                             <p className="synonyms">
                                                 <b> Synonyms : </b>
-                                                {def.synonyms.join(", ")}
+                                                {/* {def.synonyms.join(", ")} */}
+                                                {def.synonyms.map((item) => {
+                                                    return <p>{item}</p>;
+                                                })}
                                             </p>
                                         )}
                                     </div>
@@ -52,4 +94,4 @@ const index = ({ meanings, word, category }) => {
     );
 };
 
-export default index;
+export default Index;
