f86f54295b0d00b4ab1d1f29e9a9dc3e0fb3059c
kiddodev050 <yt1997119@gmail.com>
2021-10-07 13:42:49 +0530

    Adding Footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const index = () => {
+    return (
+        <footer className="footer">
+            <p>
+                Made by{" "}
+                <a
+                    href="https://github.com/j471n"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    Jatin Sharma
+                </a>
+            </p>
+        </footer>
+    );
+};
+
+export default index;
